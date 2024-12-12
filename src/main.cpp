aef53f6ef05157003833771bccb33f2249f650fe
Anoof Chappangathil <anoof7262@gmail.com>
2024-12-12 11:01:09 +0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#define DEBUG   0
+
+#define PIN1    26
+#define PIN2    33
+#define PIN3    32
+#define PIN4    25
+
+#define COMMAND1    "FIRST"
+#define COMMAND2    "HOME"
+#define COMMAND3    "FIRST"
+#define COMMAND4    "HOME"
+
+#define DEBOUNCE_DELAY 500
+
+#include <Arduino.h>
+
+uint8_t pins[4] = {PIN1, PIN2, PIN3, PIN4};
+String commands[4] = {COMMAND1, COMMAND2, COMMAND3, COMMAND4};
+
+uint32_t lastUpdateMillis = 0;
+
+void readPins(){
+  for (int i = 0; i < 4; i++) {
+    if (digitalRead(pins[i]) == LOW) {
+      if (millis() - lastUpdateMillis < DEBOUNCE_DELAY) { return; }
+      Serial2.println(commands[i]);
+      if (DEBUG) { Serial.println("Pin " + String(i) + " pressed, sending command: " + commands[i]);} 
+      lastUpdateMillis = millis();
+    }
+  }
+}
+
+void io_init() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(pins[i], INPUT_PULLUP);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  io_init();
+}
+
+void loop(){
+  readPins();
+}
